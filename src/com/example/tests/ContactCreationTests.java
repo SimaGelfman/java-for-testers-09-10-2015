e779ede7cdbf72bff8bc0667cc6d8ff59d54afb9
SimaGelfman <GelfmanSima@gmail.com>
2015-12-08 17:09:25 +0200

    add load method for contacts
    add load method for contacts

--- src/com/example/tests/ContactCreationTests.java
+++ src/com/example/tests/ContactCreationTests.java
@@ -1,16 +1,31 @@
 package com.example.tests;
 
+import static com.example.tests.ContactDataGenerator.loadContactsFromCsvFile;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.example.utils.SortedListOf;
 
 public class ContactCreationTests extends TestBase{ 
 
+@DataProvider 
+public Iterator<Object[]> contactsFromFile() throws IOException{
+	return wrapsContactsForDataProvider(loadContactsFromCsvFile(new File("contacts.txt"))).iterator();
+}
 
-@Test(dataProvider = "randomValidContactGenerator")
+	
+
+
+
+@Test(dataProvider = "contactsFromFile")
 public void testContactCreationWithValidData(ContactData contact) throws Exception {
 	
 	//save old state
--- src/com/example/tests/ContactDataGenerator.java
+++ src/com/example/tests/ContactDataGenerator.java
@@ -1,11 +1,17 @@
 package com.example.tests;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import com.thoughtworks.xstream.XStream;
+
 import static com.example.tests.GroupsDataGenerator.generateRandomString;
 
 public class ContactDataGenerator {
@@ -36,11 +42,21 @@ public class ContactDataGenerator {
 	
 	
 
-	private static void saveContactsToXmlFile(List<ContactData> contacts, File file) {
-		// TODO Auto-generated method stub
-		
+	private static void saveContactsToXmlFile(List<ContactData> contacts, File file) throws IOException {
+		XStream xstream =  new XStream();
+		xstream.alias("contact", ContactData.class);
+		String xml = xstream.toXML(contacts);
+		FileWriter writer = new FileWriter(file);
+		writer.write(xml);
+		writer.close();
 	}
 
+	
+	public static List<ContactData> loadContactsFromXmlFile(File file) {
+		XStream xstream = new XStream();
+		xstream.alias("contact", ContactData.class);
+		return (List<ContactData>) xstream.fromXML(file);
+	}
 
 
 	private static void saveContactsToCvsFile(List<ContactData> contacts, File file) throws IOException {
@@ -54,6 +70,34 @@ public class ContactDataGenerator {
 		}
 		writer.close();		
 	}
+		
+	public static List<ContactData> loadContactsFromCsvFile(File file) throws IOException {
+		List<ContactData> contacts = new ArrayList<ContactData>();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		String line = bufferedReader.readLine();
+		String parts[];
+		while(line != null){
+			parts = line.split(",");
+			ContactData contact = new ContactData()
+					.withFirstName(parts[0])
+					.withLastName(parts[1])
+					.withFirstAdress(parts[2])
+					.withHomePhoneNumber(parts[3])
+					.withMobilePhoneNumber(parts[4])
+					.withWorkPhoneNumber(parts[5])
+					.withFirstAdress(parts[6])
+					.withSecondEmail(parts[7])
+					.withGroupName(parts[8])
+					.withBirthDay(parts[9])
+					.withBirthMonth(parts[10])
+					.withBirthYear(parts[11])
+					.withSecondAdress(parts[12])
+					.withHomeNumber(parts[13]);
+			contacts.add(contact);
+			line = bufferedReader.readLine();
+		}
+		return contacts;
+	}
 
 	public static List<ContactData> generateRandomContacts(int amount) {
 		List<ContactData> list = new ArrayList<ContactData>();
@@ -66,6 +110,7 @@ public class ContactDataGenerator {
 								.withFirstAdress(generateRandomString())
 								.withFirstEmail("mail" + rnd.nextInt() + "@" + "gmail.com")
 								.withSecondEmail("mail" + rnd.nextInt() + "@" + "yahoo.com")
+								.withGroupName(null)
 								.withHomePhoneNumber(Integer.toString(rnd.nextInt(900000000) +100000000))
 								.withMobilePhoneNumber(Integer.toString(rnd.nextInt(900000000) +100000000))
 								.withWorkPhoneNumber(Integer.toString(rnd.nextInt(900000000) +100000000))
